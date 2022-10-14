788c2a79f778b837dcc162daf7e991e4d44e3a48
victor-at-wiris <victor.santiago@wiris.com>
2022-10-14 15:04:54 +0200

    feat: implement `new` function that creates `WordSet` from yaml

--- src/words/mod.rs
+++ src/words/mod.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct Word {
     word: String,
     cathegory: String,
@@ -5,7 +6,7 @@ pub struct Word {
 }
 
 impl Word {
-    pub fn new(word: &str, cathegory: &str, translations: &[&str]) -> Self {
+    pub fn new(word: &str, cathegory: &str, translations: Vec<&str>) -> Self {
         Self {
             word: word.to_string(),
             cathegory: cathegory.to_string(),
@@ -22,6 +23,7 @@ pub enum Error {
     NotImplemented,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct WordSet {
     name: String,
     words: Vec<Word>,
@@ -30,7 +32,7 @@ pub struct WordSet {
 impl WordSet {
     /// This funciton will take ownership of `self` and register a word in `self.words` by
     /// pushing it to the vector.
-    pub fn register_word(mut self, word: Word) {
+    pub fn register_word(&mut self, word: Word) {
         self.words.push(word)
     }
 
@@ -39,5 +41,52 @@ impl WordSet {
             .iter()
             .find(|w| w.word == word)
             .ok_or(Error::NotImplemented)
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.to_string()
+    }
+
+    pub fn new(name: &str) -> Self {
+        let path_to_yaml = format!("resources/{}.yaml", name);
+        let source = std::fs::read_to_string(path_to_yaml).unwrap();
+        let yamls = yaml_rust::YamlLoader::load_from_str(source.as_str()).unwrap();
+        let mut set = Self {
+            name: name.to_string(),
+            words: vec![],
+        };
+        for yaml in yamls {
+            let new_word = Word::new(
+                yaml["original"].as_str().unwrap(),
+                name,
+                yaml["translations"]
+                    .as_vec()
+                    .unwrap()
+                    .into_iter()
+                    .map(|c| c.as_str().unwrap())
+                    .collect::<Vec<&str>>(),
+            );
+            set.register_word(new_word)
+        }
+        set
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Word, WordSet};
+
+    #[test]
+    fn test_register_word() {
+        let set = WordSet::new("transports");
+        let expected = WordSet {
+            name: String::from("transports"),
+            words: vec![Word {
+                word: String::from("машина"),
+                cathegory: String::from("transports"),
+                translations: vec![String::from("coche"), String::from("auto")],
+            }],
+        };
+        pretty_assertions::assert_eq!(set, expected)
     }
 }
