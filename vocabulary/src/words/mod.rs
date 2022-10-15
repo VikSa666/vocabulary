a30484972de38df552c715d87e8359183929a6f3
Víctor Santiago <victorsantiago.matesub@gmail.com>
2022-10-15 20:36:31 +0200

    feat: add logig to write in yaml files and add words

--- vocabulary/src/words/mod.rs
+++ vocabulary/src/words/mod.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 #[derive(Debug, PartialEq)]
 pub struct Word {
     pub word: String,
@@ -52,7 +54,7 @@ impl WordSet {
     }
 
     pub fn new(name: &str) -> Self {
-        let path_to_yaml = format!("../../vocabulary/resources/{}.yaml", name);
+        let path_to_yaml = format!("resources/{}.yaml", name);
         let source = std::fs::read_to_string(path_to_yaml).unwrap();
         let yamls = yaml_rust::YamlLoader::load_from_str(source.as_str()).unwrap();
         let mut set = Self {
@@ -74,11 +76,90 @@ impl WordSet {
         }
         set
     }
+
+    pub fn write_in_file(&self) -> Result<(), std::io::Error> {
+        let path = format!("resources/{}.yaml", self.name);
+        let out_dir = &std::path::PathBuf::from(path.to_string());
+        let out = std::fs::File::create(out_dir)?;
+        write!(
+            &out,
+            "{}",
+            self.get_word_refs()
+                .iter()
+                .map(|w| w.to_string())
+                .collect::<Vec<_>>()
+                .join("\n")
+        )
+    }
+}
+
+impl std::fmt::Display for Word {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "\n---\noriginal: {}\ntranslations: [{}]",
+            self.word,
+            self.translations.join(", ")
+        )
+    }
+}
+
+fn ask_new_word() {
+    let mut s = String::new();
+    print!("Cathegory: ");
+    let _ = std::io::Write::flush(&mut std::io::stdout());
+    std::io::stdin()
+        .read_line(&mut s)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    let cathegory = s.clone();
+    s = String::new();
+    print!("Word: ");
+    let _ = std::io::Write::flush(&mut std::io::stdout());
+    std::io::stdin()
+        .read_line(&mut s)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    let word = s.clone();
+    s = String::new();
+    print!("Translations: ");
+    let _ = std::io::Write::flush(&mut std::io::stdout());
+    std::io::stdin()
+        .read_line(&mut s)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    let translations = s
+        .clone()
+        .split_whitespace()
+        .map(|str| str.to_string())
+        .collect::<Vec<String>>();
+    let mut word_set = WordSet::new(&cathegory);
+    word_set.register_word(Word::new(
+        word.as_str(),
+        cathegory.as_str(),
+        translations.iter().map(|s| s.as_str()).collect::<Vec<_>>(),
+    ));
+    word_set.write_in_file().unwrap();
 }
 
 #[cfg(test)]
 mod test {
-    use super::{Word, WordSet};
+    use super::{ask_new_word, Word, WordSet};
 
     #[test]
     fn test_register_word() {
@@ -96,5 +177,11 @@ mod test {
             }],
         };
         pretty_assertions::assert_eq!(set, expected)
+    }
+
+    #[test]
+    fn test_write_word() {
+        ask_new_word()
+        // std::fs::File::open("resources/test.yaml").unwrap();
     }
 }
